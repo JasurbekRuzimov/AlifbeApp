d01606423471b81c380bd2d879d86bb732660fe8
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2023-12-25 16:47:03 +0500

    Splash update

---
+++ app/src/main/java/uz/jasurbekruzimov/alifbeapp/Splash.java
@@ -0,0 +1,22 @@
+package uz.jasurbekruzimov.alifbeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(Splash.this, MainActivity.class));
+            finish();
+        }, 1500);
+
+    }
+}
